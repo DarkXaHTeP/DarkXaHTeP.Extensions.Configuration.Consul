9379a3ffd8ce4d91feb295c69cea5de6d05b51ea
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-11 03:17:32 +0300

    Added placeholder for provider

---
+++ ConsulConfiguration/ConsulConfigurationProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace DarkXaHTeP.Extensions.Configuration.Consul
+{
+    public class ConsulConfigurationProvider: ConfigurationProvider
+    {
+        private readonly string _consulAddress;
+        private readonly HttpClient _httpClient;
+
+        internal ConsulConfigurationProvider(string consulAddress, HttpClient httpClient)
+        {
+            _consulAddress = consulAddress;
+            _httpClient = httpClient;
+        }
+
+        public override void Load()
+        {
+            
+        }
+    }
+}
