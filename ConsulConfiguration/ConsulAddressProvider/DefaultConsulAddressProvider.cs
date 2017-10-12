f45fc586df9c04db5dc48d678649992f64e1e672
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-13 01:51:17 +0300

    Extracted address-related logic into ConsulAddressProvider

---
+++ ConsulConfiguration/ConsulAddressProvider/DefaultConsulAddressProvider.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace DarkXaHTeP.Extensions.Configuration.Consul.ConsulAddressProvider
+{
+    public class DefaultConsulAddressProvider: IConsulAddressProvider
+    {
+        public string GetConsulBaseAddress(string host, uint? port)
+        {
+            var consulBaseAddress = ResolveBaseAddress(host, port);
+            return $"http://{consulBaseAddress}";
+        }
+        
+        private string ResolveBaseAddress(string host, uint? port)
+        {
+            var candidates = new[]
+            {
+                CreateAddress(host, port),
+                GetEnvAddress(),
+                "localhost:8500"
+            };
+
+            return candidates.First(addr => !String.IsNullOrEmpty(addr));
+        }
+
+        private string CreateAddress(string host, uint? port)
+        {
+            if (String.IsNullOrEmpty(host) || !port.HasValue)
+            {
+                return null;
+            }
+
+            return $"{host}:{port}";
+        }
+
+        private string GetEnvAddress()
+        {
+            string host = Environment.GetEnvironmentVariable("CONSUL_HOST");
+            string portString = Environment.GetEnvironmentVariable("CONSUL_PORT");
+            
+            if (String.IsNullOrEmpty(host) || String.IsNullOrEmpty(portString))
+            {
+                return null;
+            }
+
+            if (!UInt32.TryParse(portString, out var port))
+            {
+                throw new ArgumentException("Cannot parse ENV variable \"CONSUL_PORT\". It's value is not a valid port");
+            }
+
+            return CreateAddress(host, port);
+        }        
+    }
+}
---
+++ ConsulConfiguration/ConsulAddressProvider/IConsulAddressProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DarkXaHTeP.Extensions.Configuration.Consul.ConsulAddressProvider
+{
+    public interface IConsulAddressProvider
+    {
+        string GetConsulBaseAddress(string host, uint? port);
+    }
+}
