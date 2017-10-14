ebfe352d1e5f3c45fb8463f10f95d0a5816409ea
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-14 15:38:07 +0300

    Added parser tests

---
+++ ConsulConfiguration.Test/ConfigurationParserTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using DarkXaHTeP.Extensions.Configuration.Consul.Parser;
+using Xunit;
+
+namespace DarkXaHTeP.Extensions.Configuration.Consul.Test
+{
+    public class ConfigurationParserTest
+    {
+        [Theory]
+        [InlineData("testkey", "testkey")]
+        [InlineData("subkey/testkey", "subkey:testkey")]
+        [InlineData("testkey[0]", "testkey:0")]
+        [InlineData("testkey[111]", "testkey:111")]
+        [InlineData("subkey[0]/testkey", "subkey:0:testkey")]
+        public void ShouldParseKeys(string consulKey, string configurationKey)
+        {
+            const string consulPrefix = "app";
+            const string testValue = "testvalue";
+
+            var consulData = new Dictionary<string, string>
+            {
+                {$"{consulPrefix}/{consulKey}", testValue}
+            };
+
+            var parser = new ConfigurationParser(consulPrefix);
+
+            var result = parser.ParseConfiguration(consulData);
+
+            Assert.Single(result);
+            Assert.True(result.ContainsKey(configurationKey), "Parsed configuration doesn't contain key");
+            Assert.Equal(testValue, result[configurationKey]);
+        }
+    }
+}
