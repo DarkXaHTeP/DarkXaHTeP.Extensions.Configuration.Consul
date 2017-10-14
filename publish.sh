a91b5e943e209205d3d7ea28c49b80924a903f24
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-14 23:40:59 +0300

    Added replace to publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-dotnet pack ./ConsulConfiguration.Test/ConsulConfiguration.csproj -o out
+dotnet pack ./ConsulConfiguration.Test/ConsulConfiguration.csproj -o ../release
+
+sed -i '' "s%{NUGET_SOURCE}%$NUGET_SOURCE%g" NuGet.config
+sed -i '' "s%{NUGET_USERNAME}%$NUGET_USERNAME%g" NuGet.config
+sed -i '' "s%{NUGET_API_KEY}%$NUGET_API_KEY%g" NuGet.config
+
+
+dotnet nuget push ./release/*.nupkg -k $NUGET_API_KEY -s NuGetFeed
