356b42d8279971ac75a3cc0d5b8ac7bfaf0a25ed
Volodymyr Osmak <darkxahtep@gmail.com>
2017-10-14 15:03:51 +0300

    Added ConfigurationParser and interface

---
+++ ConsulConfiguration/ConfigurationParser/ConfigurationParser.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace DarkXaHTeP.Extensions.Configuration.Consul.ConfigurationParser
+{
+    public class ConfigurationParser: IConfigurationParser
+    {
+        private readonly Regex _arrayRegex = new Regex(@"\[\d+\]$");
+        private readonly string _keyDelimiter = ":";
+        private readonly string _prefix;
+
+        public ConfigurationParser(string prefix)
+        {
+            _prefix = prefix;
+        }
+
+        public Dictionary<string, string> ParseConfiguration(Dictionary<string, string> consulKvDictionary)
+        {
+            Dictionary<string, string> results = consulKvDictionary
+                .Select(kv => ParseKey(kv.Key, kv.Value))
+                .ToDictionary(kv => kv.Key, kv => kv.Value);
+
+            return results;
+        }
+
+        private KeyValuePair<string, string> ParseKey(string key, string value)
+        {
+            var keyParts = RemovePrefix(key).Split(new[] {"/"}, StringSplitOptions.RemoveEmptyEntries);
+
+            if (keyParts.Length == 0)
+            {
+                throw new ArgumentException($"Consul key {key} is incorrect", nameof(key));
+            }
+
+            var configurationKeyParts = keyParts.Select(ConvertKeyPart);
+            
+            var configurationKey = String.Join(_keyDelimiter, configurationKeyParts);
+            
+            return new KeyValuePair<string, string>(configurationKey, value);
+        }
+
+        private string RemovePrefix(string key)
+        {
+            return key.Substring(_prefix.Length);
+        }
+
+        private string ConvertKeyPart(string keyPrefix)
+        {
+            if (_arrayRegex.IsMatch(keyPrefix))
+            {
+                var openBracketIndex = keyPrefix.LastIndexOf('[');
+                var replaced = keyPrefix
+                                   .Remove(openBracketIndex, 1)
+                                   .Insert(openBracketIndex, _keyDelimiter)
+                                   .Substring(0, keyPrefix.Length - 1);
+
+                return replaced;
+
+            }
+
+            return keyPrefix;
+        }
+    }
+}
---
+++ ConsulConfiguration/ConfigurationParser/IConfigurationParser.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DarkXaHTeP.Extensions.Configuration.Consul.ConfigurationParser
+{
+    public interface IConfigurationParser
+    {
+        Dictionary<string, string> ParseConfiguration(Dictionary<string, string> consulKvDictionary);
+    }
+}
