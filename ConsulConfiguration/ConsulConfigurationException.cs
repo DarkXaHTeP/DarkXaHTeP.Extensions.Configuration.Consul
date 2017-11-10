cad985d6908a109e6e745d70e2af8b3a5155c26e
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-10 02:04:44 +0200

    Added ConsulConfigurationException

---
+++ ConsulConfiguration/ConsulConfigurationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DarkXaHTeP.Extensions.Configuration.Consul
+{
+    public class ConsulConfigurationException: Exception
+    {
+        public ConsulConfigurationException(string message, Exception innerException): base(message, innerException)
+        {
+        }
+
+        public ConsulConfigurationException(string message): base(message)
+        {
+        }
+    }
+}
