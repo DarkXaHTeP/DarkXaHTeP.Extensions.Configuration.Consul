fe902088b3d213a16c82ced0516243458b8cc27d
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-11 00:58:30 +0200

    Fixed publish script to push to nuget.org

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 
 dotnet pack ./ConsulConfiguration/ConsulConfiguration.csproj -c release -o ../release
 
-dotnet nuget push ./release/*.nupkg -k $NUGET_API_KEY -s NuGetFeed
+dotnet nuget push ./release/*.nupkg -k $NUGET_API_KEY -s https://www.nuget.org/api/v2/package
