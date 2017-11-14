0aa27d02c50228f54f7a2ee0de6a4a60834c782e
Volodymyr Osmak <volodymyr.osmak@jet.com>
2017-11-14 19:35:44 +0200

    Added generate coverage script

---
+++ appveyor/generate-coverage.bat
@@ -0,0 +1,12 @@
+mkdir .\coverage
+
+.\opencover\OpenCover.Console.exe ^
+  -target:".\dotnet\dotnet.exe" ^
+  -targetargs:"test -f netcoreapp2.0 -c Release ..\ConsulConfiguration.Test\ConsulConfiguration.Test.csproj" ^
+  -mergeoutput ^
+  -hideskipped:File ^
+  -output:coverage/coverage.xml ^
+  -oldStyle ^
+  -filter:"+[DarkXaHTeP.Extensions.Configuration.Consul*]* -[DarkXaHTeP.Extensions.Configuration.Consul.Test*]*" ^
+  -searchdirs:"../ConsulConfiguration.Test/bin/Release/netcoreapp2.0" ^
+  -register:user
