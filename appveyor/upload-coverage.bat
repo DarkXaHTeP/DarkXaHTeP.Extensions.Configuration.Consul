465a116bc770dd8466b2d2480ed00d549348f020
Volodymyr Osmak <volodymyr.osmak@jet.com>
2017-11-14 21:14:25 +0200

    Removed commit and build info from coverage upload

--- appveyor/upload-coverage.bat
+++ appveyor/upload-coverage.bat
@@ -2,7 +2,4 @@ echo "Uploading coverage"
 IF "%COVERALLS_TOKEN%"=="" echo "Coveralls token variable is NOT defined"
 .\coveralls\tools\csmacnz.Coveralls.exe ^
  --opencover -i .\coverage\coverage.xml ^
- --repoToken %COVERALLS_TOKEN% ^
- --commitId %APPVEYOR_REPO_COMMIT% --commitBranch %APPVEYOR_REPO_BRANCH% ^
- --commitAuthor %APPVEYOR_REPO_COMMIT_AUTHOR% --commitEmail %APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL% ^
-  --commitMessage %APPVEYOR_REPO_COMMIT_MESSAGE% --jobId %APPVEYOR_JOB_ID%
+ --repoToken %COVERALLS_TOKEN%
