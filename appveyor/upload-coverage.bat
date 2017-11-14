f1cf5b8146a439f8bd2a70ff759b951f397b12cd
Volodymyr Osmak <darkxahtep@gmail.com>
2017-11-15 01:24:19 +0200

    Fixed issue with source files not showing on coverage report

--- appveyor/upload-coverage.bat
+++ appveyor/upload-coverage.bat
@@ -2,4 +2,5 @@ echo "Uploading coverage"
 IF "%COVERALLS_TOKEN%"=="" echo "Coveralls token variable is NOT defined"
 .\coveralls\tools\csmacnz.Coveralls.exe ^
  --opencover -i .\coverage\coverage.xml ^
- --repoToken %COVERALLS_TOKEN%
+ --repoToken %COVERALLS_TOKEN% ^
+ --useRelativePaths
