384edd1f981f11ddf02f16353a692ee2c1e44e8d
Volodymyr Osmak <volodymyr.osmak@jet.com>
2017-11-14 21:09:48 +0200

    Fixed env vars in upload-coverage script

--- appveyor/upload-coverage.bat
+++ appveyor/upload-coverage.bat
@@ -3,6 +3,6 @@ IF "%COVERALLS_TOKEN%"=="" echo "Coveralls token variable is NOT defined"
 .\coveralls\tools\csmacnz.Coveralls.exe ^
  --opencover -i .\coverage\coverage.xml ^
  --repoToken %COVERALLS_TOKEN% ^
- --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH ^
- --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL ^
-  --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID
+ --commitId %APPVEYOR_REPO_COMMIT% --commitBranch %APPVEYOR_REPO_BRANCH% ^
+ --commitAuthor %APPVEYOR_REPO_COMMIT_AUTHOR% --commitEmail %APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL% ^
+  --commitMessage %APPVEYOR_REPO_COMMIT_MESSAGE% --jobId %APPVEYOR_JOB_ID%
