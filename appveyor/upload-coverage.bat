da476aa3eee3b91d2ddef74a2e5ea0db89cb9b2b
Volodymyr Osmak <volodymyr.osmak@jet.com>
2017-11-14 21:04:27 +0200

    Added upload-coverage script

--- appveyor/upload-coverage.bat
+++ appveyor/upload-coverage.bat
@@ -1,2 +1,8 @@
 echo "Uploading coverage"
-IF "%COVERALLS_TOKEN%"=="" ECHO Coveralls token variable is NOT defined
+IF "%COVERALLS_TOKEN%"=="" echo "Coveralls token variable is NOT defined"
+.\coveralls\tools\csmacnz.Coveralls.exe ^
+ --opencover -i .\coverage\coverage.xml ^
+ --repoToken %COVERALLS_TOKEN% ^
+ --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH ^
+ --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL ^
+  --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID
